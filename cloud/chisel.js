3e6349f2c083011c9b29c5e1ea5347c2b1692f25
Nikita Golovin <golovin.nik@gmail.com>
2017-03-16 05:47:41 +0300

    Chisel — Cloud code — setup ACL when modifying collaborations and add models

--- cloud/chisel.js
+++ cloud/chisel.js
@@ -145,3 +145,187 @@ Parse.Cloud.define("deleteSite", (request, response) => {
   
     .catch(error => response.error("Could not delete site: " + JSON.stringify(error, null, 2)));
 });
+
+
+const ROLE_ADMIN = "ADMIN";
+
+Parse.Cloud.define("onCollaborationModify", (request, response) => {
+  if (!request.user) {
+    response.error('You must be authorized!');
+    return;
+  }
+   
+  Parse.Cloud.useMasterKey();
+  
+  let deleting = request.params.deleting;
+  
+  let collab, site, user, role, owner, collabACL;
+  
+  promisify(
+    new Parse.Query("Collaboration")
+      .get(request.params.collabId)
+  )
+    .then(p_collab => {
+      collab = p_collab;
+      
+      site = collab.get('site');
+      user = collab.get('user');
+      role = collab.get('role');
+      
+      return promisify(site.fetch());
+    })
+  
+    .then(() => {
+      //ACL for collaborations
+      owner = site.get('owner');
+      
+      collabACL = collab.getACL();
+      if (!collabACL)
+        collabACL = new Parse.ACL(owner);
+      
+      return promisify(
+        new Parse.Query('Collaboration')
+          .equalTo('site', site)
+          .find());
+    })
+    
+    .then(collabs => {
+      for (let tempCollab of collabs) {
+        //set ACL for others collab
+        let tempCollabACL = tempCollab.getACL();
+        if (!tempCollabACL)
+          tempCollabACL = new Parse.ACL(owner);
+  
+        let tempUser = tempCollab.get('user');
+        if (tempUser == user)
+          continue;
+    
+        tempCollabACL.setReadAccess(user, !deleting && role == ROLE_ADMIN);
+        tempCollabACL.setWriteAccess(user, !deleting && role == ROLE_ADMIN);
+    
+        tempCollab.setACL(tempCollabACL);
+        tempCollab.save();
+    
+        //set ACL for current collab
+        if (!deleting) {
+          let tempRole = tempCollab.get('role');
+          collabACL.setReadAccess(tempUser, tempRole == ROLE_ADMIN);
+          collabACL.setWriteAccess(tempUser, tempRole == ROLE_ADMIN);
+        }
+      }
+  
+      if (!deleting) {
+        collabACL.setReadAccess(user, true);
+        collabACL.setWriteAccess(user, true);
+        collab.setACL(collabACL);
+        collab.save();
+      }
+    })
+    
+    .then(() => {
+      //ACL for site
+      let siteACL = site.getACL();
+      if (!siteACL)
+        siteACL = new Parse.ACL(owner);
+  
+      siteACL.setReadAccess(user, !deleting);
+      siteACL.setWriteAccess(user, !deleting && role == ROLE_ADMIN);
+      site.setACL(siteACL);
+      site.save();
+  
+      //ACL for models and content items
+  
+      return promisify(
+        new Parse.Query('Model')
+          .equalTo('site', site)
+          .find());
+    })
+    
+    .then(models => {
+      for (let model of models) {
+        let modelACL = model.getACL();
+        if (!modelACL)
+          modelACL = new Parse.ACL(owner);
+    
+        modelACL.setReadAccess(user, !deleting);
+        modelACL.setWriteAccess(user, !deleting && role == ROLE_ADMIN);
+        model.setACL(modelACL);
+        model.save();
+    
+        /*
+         let Content = Parse.Object.extend(model.tableName);
+         let contentACL = Content.getACL();
+         contentACL.setReadAccess(collab.user.origin, !deleting);
+         contentACL.setWriteAccess(collab.user.origin, !deleting && (collab.role == ROLE_ADMIN || collab.role == ROLE_DEVELOPER));
+         Content.setACL(contentACL);
+         Content.save();
+         */
+      }
+    })
+    
+    .then(() => response.success('ACL setup ends!'))
+    
+    .catch(e => response.error(e));
+});
+
+
+Parse.Cloud.define("onModelAdd", (request, response) => {
+  if (!request.user) {
+    response.error('You must be authorized!');
+    return;
+  }
+  
+  Parse.Cloud.useMasterKey();
+  
+  let model, site, owner, modelACL;
+  
+  promisify(
+    new Parse.Query("Model")
+      .get(request.params.modelId)
+  )
+    .then(p_model => {
+      model = p_model;
+      
+      site = model.get('site');
+      return promisify(site.fetch());
+    })
+    
+    .then(() => {
+      //ACL for collaborations
+      owner = site.get('owner');
+      modelACL = new Parse.ACL(owner);
+      
+      return promisify(
+        new Parse.Query('Collaboration')
+          .equalTo('site', site)
+          .find());
+    })
+    
+    .then(collabs => {
+      for (let collab of collabs) {
+        let user = collab.get('user');
+        let role = collab.get('role');
+  
+        modelACL.setReadAccess(user, true);
+        modelACL.setWriteAccess(user, role == ROLE_ADMIN);
+      }
+  
+      model.setACL(modelACL);
+      model.save();
+    })
+    
+    .then(() => {
+      /*
+       let Content = Parse.Object.extend(model.tableName);
+       let contentACL = Content.getACL();
+       contentACL.setReadAccess(collab.user.origin, !deleting);
+       contentACL.setWriteAccess(collab.user.origin, !deleting && (collab.role == ROLE_ADMIN || collab.role == ROLE_DEVELOPER));
+       Content.setACL(contentACL);
+       Content.save();
+       */
+    })
+    
+    .then(() => response.success('ACL setup ends!'))
+    
+    .catch(e => response.error(e));
+});
