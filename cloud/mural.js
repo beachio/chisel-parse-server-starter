52f75b3c4ff0a274f31dc6838a7714ac874a0d7b
Alfred Takala <alfredtakala@yandex.com>
2021-01-13 23:11:02 +0800

    update published/draft record on parse cloud

--- cloud/mural.js
+++ cloud/mural.js
@@ -6,14 +6,13 @@ Parse.Cloud.define("generateTicket", async request => {
   if (!email)
     throw 'There is no email param!';
   const participantQuery = new Parse.Query(PARTTICIPANT_MODEL);
-  participantQuery.equalTo('t__status', 'Published');
   participantQuery.equalTo('Email', email);
-  const currentParticipant = await participantQuery.first({useMasterKey: true});
-  if (!currentParticipant) 
+  const currentParticipant = await participantQuery.find({useMasterKey: true});
+  if (!currentParticipant || currentParticipant.length < 1) 
     throw 'There is no participant with the given email!';
 
   // If the record already has ticket information, no need to regenerate again.
-  if (currentParticipant.get('ticket')) return currentParticipant.get('ticket');
+  if (currentParticipant[0].get('ticket')) return currentParticipant[0].get('ticket');
 
   // generate the ticket, currently in order
   const countQuery = new Parse.Query(PARTTICIPANT_MODEL);
@@ -23,44 +22,50 @@ Parse.Cloud.define("generateTicket", async request => {
   
   const newTicketNumber = ('000000' + (count + 1)).slice(-6);
 
-  currentParticipant.set('ticket', newTicketNumber);
-  await currentParticipant.save({useMasterKey: true});
+  for (let i = 0; i < currentParticipant.length; i++) {
+    currentParticipant[i].set('ticket', newTicketNumber);
+    await currentParticipant[i].save({useMasterKey: true});
+  }
 
   return newTicketNumber;
 });
 
 
 Parse.Cloud.define("claimPoints", async request => {
+  let i;
   const {code, participant} = request.params;
   if (!code || !participant)
     throw 'Insufficient Data!';
   const challengeQuery = new Parse.Query(CHALLENGE_MODEL);
-  challengeQuery.equalTo('t__status', 'Published');
+//  challengeQuery.equalTo('t__status', 'Published');
   challengeQuery.equalTo('Enabled', true);
   challengeQuery.equalTo('Code', code);
-  const currentChallenge = await challengeQuery.first({useMasterKey: true});
-  if (!currentChallenge)
+  const currentChallenge = await challengeQuery.find({useMasterKey: true});
+  if (!currentChallenge || currentChallenge.length < 1)
     throw 'There is no challenge with the given code!';
-  console.log('currentChallenge', currentChallenge.get('Redeem_List'));
 
   const participantQuery = new Parse.Query(PARTTICIPANT_MODEL);
-  participantQuery.equalTo('t__status', 'Published');
+  // participantQuery.equalTo('t__status', 'Published');
   participantQuery.equalTo('Email', participant);
-  const currentParticipant = await participantQuery.first({useMasterKey: true});
-  if (!currentParticipant) 
+  const currentParticipant = await participantQuery.find({useMasterKey: true});
+  if (!currentParticipant || currentParticipant.length < 1) 
     throw 'There is no participant with the given email!';
 
   
   // check if participant already claim points of this challenge
-  const redeemList = currentChallenge.get('Redeem_List') || [];
-  const redeemIndex = redeemList.findIndex(participant => participant.id === currentParticipant.id);
+  const redeemList = currentChallenge[0].get('Redeem_List') || [];
+  const redeemIndex = redeemList.findIndex(participant => participant.id === currentParticipant[0].id);
 
   // If no previous claim record of the participant, increase the points and append participant to redeem list
   if (redeemIndex === -1) {
-    currentParticipant.set('Points', currentParticipant.get('Points') + currentChallenge.get('Points'))
-    await currentParticipant.save();
+    for (i = 0; i < currentParticipant.length; i++) {
+      currentParticipant[i].set('Points', currentParticipant[i].get('Points') + currentChallenge[0].get('Points'))
+      await currentParticipant[i].save();
+    }
 
-    currentChallenge.set('Redeem_List', [...redeemList, currentParticipant]);
-    await currentChallenge.save();
+    for (i = 0; i < currentChallenge.length; i++) {
+      currentChallenge[i].set('Redeem_List', [...redeemList, ...currentParticipant]);
+      await currentChallenge[i].save();
+    }
   }
 });
